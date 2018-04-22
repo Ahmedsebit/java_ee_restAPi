0dcb473b988e29617e3e03fdcbc97f1c2cd03aed
Ahmed.sebit <ahmed.yusuf@andela.com>
2018-04-22 16:39:07 +0300

    ADD injection

--- bookstore-back/src/main/java/com/pluralsight/bookstore/model/repository/BookRepository.java
+++ bookstore-back/src/main/java/com/pluralsight/bookstore/model/repository/BookRepository.java
@@ -1,5 +1,6 @@
 package com.pluralsight.bookstore.model.repository;
 
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
@@ -8,6 +9,8 @@ import javax.validation.constraints.NotNull;
 
 import java.util.List;
 import com.pluralsight.bookstore.model.Book;
+import com.pluralsight.bookstore.model.util.NumberGenerator;
+import com.pluralsight.bookstore.model.util.TextUtil;
 
 @Transactional(Transactional.TxType.SUPPORTS)
 public class BookRepository{
@@ -15,12 +18,20 @@ public class BookRepository{
     @PersistenceContext(unitName = "bookStorePU")
     private EntityManager em;
 
+    @Inject
+    public TextUtil textUtil;
+
+    @Inject
+    public NumberGenerator generator;
+
     public Book find(@NotNull Long id){
         return em.find(Book.class, id);
     }
 
     @Transactional(Transactional.TxType.REQUIRED)
     public Book create(@NotNull Book book){
+        book.setTitle(textUtil.sanitize(book.getTitle()));
+        book.setIsbn(generator.generateNumber());
         em.persist(book);
         return book;
     }
---
+++ bookstore-back/src/main/java/com/pluralsight/bookstore/model/util/NumberGenerator.java
@@ -0,0 +1,5 @@
+package com.pluralsight.bookstore.model.util;
+
+public interface NumberGenerator{
+    String generateNumber();
+}
